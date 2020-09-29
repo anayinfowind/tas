efd10bb5f9d1d814b22972f8ed0040f21bd0f4d0
Infowind <om.infowind@gmail.com>
2020-09-29 19:20:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LeelooLXP Course Synchronization
+This tool is used to Synchronization Courses,Sections and Activities/Resources from moodle to Leelolxp with enrolled users and groups.
+# Admin Category
---
+++ lang/en/tool_leeloolxp_sync.php
@@ -0,0 +1,51 @@
+<?php
+
+// This file is part of
+
+//
+
+// Moodle is free software: you can redistribute it and/or modify
+
+// it under the terms of the GNU General Public License as published by
+
+// the Free Software Foundation, either version 3 of the License, or
+
+// (at your option) any later version.
+
+//
+
+// Moodle is distributed in the hope that it will be useful,
+
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+// GNU General Public License for more details.
+
+//
+
+// You should have received a copy of the GNU General Public License
+
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+/**
+
+ * Strings for component 'tool_leeloolxp_sync'.
+
+ *
+
+ * @package    tool_leeloolxp_sync
+
+ * @copyright  2020 leelolxp.com
+
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
+ */
+$string['pluginname'] = 'Sync Activities/Resources/Quizzes with Leeloo LXP';
+$string['leeloolxp_sync'] = 'Sync Activities/Resources/Quizzes with Leeloo LXP';
+$string['privacy:metadata'] = 'The plugin does not contain user data.';
+$string['leeloolxp_sync_settings'] = 'Leeloo LXP Activities and Resource Synchronizer';
+$string['leeloolxp_synclicensekey'] = 'License Key';
+$string['leeloolxp_synclicensekey_help'] = 'License Key';
