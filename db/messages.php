15ebbc846ed0fc8931971f46c82af17e4fb84c85
anayinfowind <anay.infowind@gmail.com>
2020-10-01 18:47:09 +0530

    Update messages.php

--- db/messages.php
+++ db/messages.php
@@ -17,7 +17,7 @@
 /**
  * Messaging for Wespher Departments.
  *
- * @package    leeloolxp_sync
+ * @package    tool_leeloolxp_sync
  * @copyright  2020 leeloolxp.com
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
